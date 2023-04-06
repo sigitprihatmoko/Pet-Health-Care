a8358b81288f1a9efe9c8ba5821d2d4946ccaf0a
ditsap83 <ditsap83@gmail.com>
2023-04-07 04:40:56 +0700

    perbaikan beberapa fungsi dan menambah desain

--- index.js
+++ index.js
@@ -180,135 +180,73 @@ function sortKeluhan(dataset) {
 
 var selectedRow = null
 
-function displayCheckout() {
-  nama = document.getElementById("fullName").value;
-  hewan = document.getElementById("jhewan").value;
-  jenis = document.getElementById("keluhan").value;
-  keluhan = document.getElementById("kasus").value;
-  let pasienbaru = createPasien(nama, jenis, keluhan, data)
-  dataCustomer.push(pasienbaru)
-  return pasienbaru
+function onFormSubmit(e) {
+	event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
 }
 
-function onFormSubmit() {
-  displayCheckout()
-  if (validate()) {
-    var formData = readFormData();
-    if (selectedRow == null)
-      insertNewRecord(formData);
-    else
-      updateRecord(formData);
-    resetForm();
-  }
-}
-
+//Retrieve the data
 function readFormData() {
-  var formData = {};
-  formData["fullName"] = document.getElementById("fullName").value;
-  formData["jhewan"] = document.getElementById("jhewan").value;
-  formData["keluhan"] = document.getElementById("keluhan").value;
-  formData["kasus"] = document.getElementById("kasus").value;
-  return formData;
+    var formData = {};
+    formData["namaPemilik"] = document.getElementById("namaPemilik").value;
+    formData["hewan"] = document.getElementById("hewan").value;
+    formData["jHewan"] = document.getElementById("jHewan").value;
+    formData["keluhan"] = document.getElementById("keluhan").value;
+    return formData;
 }
 
+//Insert the data
 function insertNewRecord(data) {
-  var table = document.getElementById("petList").getElementsByTagName('tbody')[0];
-  var newRow = table.insertRow(table.length);
-  cell1 = newRow.insertCell(0);
-  cell1.innerHTML = data.fullName;
-  cell2 = newRow.insertCell(1);
-  cell2.innerHTML = data.jhewan;
-  cell3 = newRow.insertCell(2);
-  cell3.innerHTML = data.keluhan;
-  cell4 = newRow.insertCell(3);
-  cell4.innerHTML = data.kasus;
-  cell4 = newRow.insertCell(4);
-  cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
+    var table = document.getElementById("dList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		        cell1.innerHTML = data.namaPemilik;
+    cell2 = newRow.insertCell(1);
+		        cell2.innerHTML = data.hewan;
+    cell3 = newRow.insertCell(2);
+            cell3.innerHTML = data.jHewan;
+    cell4 = newRow.insertCell(3);
+		        cell4.innerHTML = data.keluhan;
+    cell4 = newRow.insertCell(4);
+            cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 }
 
-function resetForm() {
-  document.getElementById("fullName").value = "";
-  document.getElementById("jhewan").value = "";
-  document.getElementById("keluhan").value = "";
-  document.getElementById("kasus").value = "";
-  selectedRow = null;
-}
-
+//Edit the data
 function onEdit(td) {
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("jhewan").value = selectedRow.cells[1].innerHTML;
-  document.getElementById("keluhan").value = selectedRow.cells[2].innerHTML;
-  document.getElementById("kasus").value = selectedRow.cells[3].innerHTML;
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("namaPemilik").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("hewan").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("jHewan").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("keluhan").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
-  selectedRow.cells[0].innerHTML = formData.fullName;
-  selectedRow.cells[1].innerHTML = formData.jhewan;
-  selectedRow.cells[2].innerHTML = formData.keluhan;
-  selectedRow.cells[3].innerHTML = formData.kasus;
+    selectedRow.cells[0].innerHTML = formData.namaPemilik;
+    selectedRow.cells[2].innerHTML = formData.hewan;
+    selectedRow.cells[1].innerHTML = formData.jHewan;
+    selectedRow.cells[3].innerHTML = formData.keluhan;
 }
 
+//Delete the data
 function onDelete(td) {
-  if (confirm('Are you sure to delete this record ?')) {
-    row = td.parentElement.parentElement;
-    document.getElementById("petList").deleteRow(row.rowIndex);
-    resetForm();
-  }
-}
-function validate() {
-  isValid = true;
-  if (document.getElementById("fullName").value == "") {
-    isValid = false;
-    document.getElementById("fullNameValidationError").classList.remove("hide");
-  } else {
-    isValid = true;
-    if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-      document.getElementById("fullNameValidationError").classList.add("hide");
-  }
-  return isValid;
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('dList').deleteRow(row.rowIndex);
+        resetForm();
+    }
 }
 
+//Reset the data
 function resetForm() {
-  document.getElementById("fullName").value = "";
-  document.getElementById("jhewan").value = "";
-  document.getElementById("keluhan").value = "";
-  document.getElementById("kasus").value = "";
-  selectedRow = null;
-}
-
-function onEdit(td) {
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("jhewan").value = selectedRow.cells[1].innerHTML;
-  document.getElementById("keluhan").value = selectedRow.cells[2].innerHTML;
-  document.getElementById("kasus").value = selectedRow.cells[3].innerHTML;
-}
-
-function updateRecord(formData) {
-  selectedRow.cells[0].innerHTML = formData.fullName;
-  selectedRow.cells[1].innerHTML = formData.jhewan;
-  selectedRow.cells[2].innerHTML = formData.keluhan;
-  selectedRow.cells[3].innerHTML = formData.kasus;
-}
-
-function onDelete(td) {
-  if (confirm('Are you sure to delete this record ?')) {
-    row = td.parentElement.parentElement;
-    document.getElementById("petList").deleteRow(row.rowIndex);
-    resetForm();
-  }
-}
-
-function validate() {
-  isValid = true;
-  if (document.getElementById("fullName").value == "") {
-    isValid = false;
-    document.getElementById("fullNameValidationError").classList.remove("hide");
-  } else {
-    isValid = true;
-    if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-      document.getElementById("fullNameValidationError").classList.add("hide");
-  }
-  return isValid;
+    document.getElementById("namaPemilik").value = '';
+    document.getElementById("hewan").value = '';
+    document.getElementById("jHewan").value = '';
+    document.getElementById("keluhan").value = '';
+    selectedRow = null;
 }
